7878d11f52961f7ca0d9e560bb940071f55421e7
Tebonus <135049203+Tebonus@users.noreply.github.com>
2025-04-29 16:29:29 +0200

    Updated button readings
    updated the button reading system to read multiple buttons, added a function where each of the buttons can have its own shortcut/key/letter (have to improve and clean up later)

--- Bluetooth1/src/main.cpp
+++ Bluetooth1/src/main.cpp
@@ -3,39 +3,59 @@
 
 #define onboardled 2 // Led na urzadzeniu
 #define przycisk 4 // Przycisk
-
-//Bluetooth
-const char* device_name = "Tymek"; // Nazwa
+#define przycisk2 16 // Button2
+#define przycisk3 17 // Button3
+// Bluetooth
+const char* device_name = "Tymek MacroPad"; // Nazwa
 bool is_connected = false; // Podloczono urzadzenie
+BleKeyboard bleKeyboard(device_name, "Tymek", 100); // (name, manufacturer, battery level %)
 
-//Przycisk
-int lastState = HIGH;
-int currentState;
-const long debounceDelay = 100; //Czas miedzy nacisnieciami
-unsigned long lastDebounce = 0;
+// Przycisk
+int lastState[] = {HIGH,HIGH,HIGH};
+int currentState[2];
+const long debounceDelay = 100; // Czas miedzy nacisnieciami
+unsigned long lastDebounce[] = {0,0,0};
 
-//Czas
+// Czas
 unsigned long lastConnectionCheck = 0;
 const unsigned long connectionCheckInterval = 500;
 
-BleKeyboard bleKeyboard(device_name, "Tymek", 100); // (name, manufacturer, battery level %)
 
-void buttonPress(uint8_t klawisze[], uint8_t numKeys); //Funkcja nacisniecia skrotu 
+
+// Functions list
+void keyboardPress(uint8_t klawisze[], uint8_t numKeys); // Transfer keys into keypresses
+void buttonPressDetect(unsigned long, int, int); // Detect button press
+void bluetoothCheckManager(unsigned long); //Manages Bluetooth
+void buttonPressScan(unsigned long); // Activates all button detections
+int chceckButtonShortcut(uint8_t, uint8_t, int);
 
 void setup() {
   pinMode(przycisk, INPUT_PULLUP);
+  pinMode(przycisk2, INPUT_PULLUP);
+  pinMode(przycisk3, INPUT_PULLUP);
   pinMode(onboardled, OUTPUT);
+  digitalWrite(onboardled, HIGH);
   Serial.begin(921600);
   bleKeyboard.begin(); // Start BLE Keyboard
   delay(500);
   Serial.println("Starting the Bluetooth Keyboard"); 
-  Serial.println("Tymoteusz Brzumowski"); 
+  Serial.println("v 0.1.1");
+  Serial.println("Tymoteusz Brzumowski");
+  delay (100);
+  digitalWrite(onboardled, LOW); 
 }
 
 void loop() {
   unsigned long currentMillis = millis();
 
-  // Sprawdzenie polaczenia
+  bluetoothCheckManager(currentMillis);
+  buttonPressScan(currentMillis);
+  //buttonPressDetect(currentMillis, przycisk2);
+}
+
+//Functions
+  //Bluetooth
+void bluetoothCheckManager(unsigned long currentMillis){
   if (currentMillis - lastConnectionCheck >= connectionCheckInterval) {
     
     lastConnectionCheck = currentMillis;
@@ -44,7 +64,7 @@ void loop() {
       if (connected && !is_connected) {
         Serial.println("Polaczono!");
         digitalWrite(onboardled, HIGH);
-        delay(250);  
+        delay(300);  
         digitalWrite(onboardled, LOW);
         is_connected = true;
       }
@@ -55,27 +75,67 @@ void loop() {
       }
   }
 
-  // Button press detection 
-  currentState = digitalRead(przycisk);
-
-  if ((currentMillis - lastDebounce) > debounceDelay) {
-    if (lastState == HIGH && currentState == LOW) {
-      //Serial.println("Przycisk nacisniety!");
-        lastDebounce=currentMillis;
-      if (bleKeyboard.isConnected()) {
-        uint8_t tab[] ={KEY_F13, 's'};
-       buttonPress(tab, sizeof(tab)/sizeof(tab[0]));
-      }
-    }
-  }
-
-  lastState = currentState;
 }
-void buttonPress (uint8_t klawisze[], uint8_t numKeys){
+  //Buttons
+void keyboardPress (uint8_t klawisze[], uint8_t numKeys){ 
     for (int i=0; i<numKeys; i++){
       bleKeyboard.press(klawisze[i]);
     }
     delay(50);
     bleKeyboard.releaseAll();
     
+}
+int chceckButtonShortcut(uint8_t tab[], uint8_t keycount, int button){
+  switch (button) {
+    case przycisk:
+     tab[keycount++] = KEY_F13;
+     tab[keycount++] ='s';
+     tab[keycount++] = 'a';
+    break;
+    default:
+     tab[keycount++] = KEY_F13;
+     tab[keycount++] ='s';
+    break;
+  }
+  return 0;
+}
+
+void buttonPressDetect(unsigned long currentMillis, int button, int number){
+  currentState[0] = digitalRead(button);
+
+  if ((currentMillis - lastDebounce[0]) > debounceDelay) {
+    if (lastState[0] == HIGH && currentState[0] == LOW) {
+      //Serial.println("Przycisk nacisniety!");
+        lastDebounce[0]=currentMillis;
+
+      if (bleKeyboard.isConnected()) {
+        
+        uint8_t tab[5];
+        uint8_t keycount=0;
+        switch (button) {
+          case przycisk:
+           tab[keycount++] = KEY_F13;
+           tab[keycount++] ='s';
+           tab[keycount++] = 'a';
+          break;
+          default:
+           tab[keycount++] = KEY_F13;
+           tab[keycount++] ='s';
+          break;
+        }
+        //chceckButtonShortcut(tab[5],keycount,button);
+       keyboardPress(tab, keycount);
+      }
+    }
+  }
+
+  lastState[0] = currentState[0];
+}
+
+void buttonPressScan(unsigned long currentMillis){
+  
+  int buttonList[] = {przycisk, przycisk2, przycisk3};
+  for (int i=0; i<sizeof(buttonList)/sizeof(buttonList[0]);i++){
+  buttonPressDetect(currentMillis, buttonList[i], i);
+  }
 }
